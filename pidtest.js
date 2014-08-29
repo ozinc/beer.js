24cf6bacb4360eac141b0de231fc74de476134ee
Baldur Gislason <baldur@oz.com>
2014-08-29 20:03:27 +0000

    Added GPIO library

--- pidtest.js
+++ pidtest.js
@@ -3,10 +3,12 @@ var pid = require('./pid');
 
 var ctrl1 = new pid(0.1, 0.01, 0.1, -1, 1, 100);
 
+// Simulate steady error state to test functional correctness of P and I factors
 for(var i = 0; i < 10; i++) {
 	console.log(ctrl1.process(90));
 }
 
+// Simulate a sweep to test functional correctness of P and D factors
 for(var i = 0; i < 10; i++) {
 	console.log(ctrl1.process(90+i));
 }
---
+++ pigpio.js
@@ -0,0 +1,53 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <baldur@foo.is> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return - Baldur Gislason
+ * ----------------------------------------------------------------------------
+ */
+
+var fs = require('fs');
+
+function pigpio(id, direction) {
+	this.id = id;
+	this.filename = '/sys/class/gpio/gpio' + id.toString() + 'value';
+	var fd = fs.openSync('/sys/class/gpio/export', 'w');
+	var buf = new Buffer(id.toString());
+	fs.writeSync(fd, buf, 0, buf.size, 0);
+	fs.closeSync(fd);
+	fd = fs.openSync('/sys/class/gpio/gpio' + id.toString() + '/direction', 'w');
+	buf = new Buffer(direction);
+	fs.writeSync(fd, buf, 0, buf.size, 0);
+	fs.closeSync(fd);
+}
+
+pigpio.prototype.set = function(value, cb) {
+	fs.writeFile(this.filename, value ? '1' : '0', cb);
+}
+
+pigpio.prototype.setSync = function(value) {
+	return fs.writeFileSync(this.filename, value ? '1' : '0');
+}
+
+pigpio.prototype.get = function(cb) {
+	fs.readFile(this.filename, function(err, data) {
+		if(err) return cb(err, null);
+		if(data == '1') {
+			cb(null, true);
+		} else {
+			cb(null, false);
+		}
+	});
+}
+
+pigpio.prototype.getSync = function() {
+	var val = readFileSync(this.filename);
+	if(val == '1') {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+module.exports = pigpio;
